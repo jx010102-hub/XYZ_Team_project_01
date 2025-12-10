33c13940b0f46aca7cd353806238f5e5ab91036d
sanghyun97 <sanjungl97@naver.com>
2025-12-10 16:53:20 +0900

    clogin

---
+++ lib/customer/c_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CLogin extends StatefulWidget {
+  const CLogin({super.key});
+
+  @override
+  State<CLogin> createState() => _CLoginState();
+}
+
+class _CLoginState extends State<CLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
